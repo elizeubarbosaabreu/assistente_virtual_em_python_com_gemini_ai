00173594d2e07eac6ab097bc4cf23015da0d9e6b
Elizeu Barbosa Abreu <elizeubarbosaabreu@yahoo.com>
2024-03-29 19:50:21 -0400

    Melhoria de Interface e Saída em Markdown com possibilidade de cópia

--- app.py
+++ app.py
@@ -8,20 +8,31 @@ from functools import cache
 from gtts import gTTS
 from playsound import playsound
 
+def ocultar_output():
+    ui.txt_resposta.hide()
+
+def exibir_output():
+    ui.txt_resposta.show()
+
 # Inicia Gemini
 @cache
-def gemini_ai(frase):
+def gemini_ai(pergunta):
+
     genai.configure(api_key=API_KEY)
     model = genai.GenerativeModel("gemini-pro")
-    response = model.generate_content(frase)
-    try:
+    response = model.generate_content(pergunta)
+    if response:
         texto = response.text
-        ui.txt_chat.setText("Resposta:\n"+texto.replace("*", " "))        
+        # Atualiza caixa de resposta
+        ui.txt_chat.setText(f"{pergunta}")
+        ui.txt_resposta.setMarkdown(f"{texto}")
+        
+        # Fala a Resposta
         cria_audio(texto.replace("*", " "))
              
-    except Exception as e:
-        ui.txt_chat.setText(f'{type(e).__name__}: {e}')        
-        cria_audio(f'{type(e).__name__}: {e}')         
+    else:
+        ui.txt_resposta.setText("Erro!")        
+        cria_audio("Aconteceu algum erro!")         
      
 
 # Funcao responsavel por falar
@@ -32,14 +43,19 @@ def cria_audio(texto):
     playsound("audio.mp3")    
 
 # Funcao responsavel por ouvir e reconhecer a fala
-def ouvir_microfone():   
+def ouvir_microfone():  
+
+    exibir_output() 
+    time.sleep(0.3)    
+
+    ui.txt_resposta.setText("")
 
     # Habilita o microfone para ouvir o usuario
     microfone = sr.Recognizer()
     with sr.Microphone() as source:
 
         # Avisa ao usuario que esta pronto para ouvir
-        cria_audio("Em que posso te ajudar?")
+        cria_audio("Diga!")
 
         # Chama a funcao de reducao de ruido disponivel na speech_recognition
         microfone.adjust_for_ambient_noise(source)
@@ -49,42 +65,59 @@ def ouvir_microfone():
 
         try:
             # Passa o audio para o reconhecedor de padroes do speech_recognition
-            frase = microfone.recognize_google(audio, language="pt-BR")
+            pergunta = microfone.recognize_google(audio, language="pt-BR")           
 
             # Após alguns segundos, retorna a frase falada 
-            gemini_ai(frase)                
+            gemini_ai(pergunta)
+
+            # Atualiza Caixa de pergunta
+            ui.txt_chat.setText(pergunta.capitalize())                
 
 
         # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
         except:
-            cria_audio("Não consegui entender...")
-            time.sleep(3)
-            ouvir_microfone()
+            cria_audio("Não entendi! Clique no botão novamente!")
             
 
 # Entrada de texto
 def enviar_chat():
 
+    exibir_output()
+    time.sleep(0.3)
+
+    ui.txt_resposta.setText("")
 
     txt = ui.txt_chat.toPlainText()
 
     if txt:
-
-        cria_audio(txt)
-
+       
+        try:
+            # Após alguns segundos, retorna a frase falada                   
+            gemini_ai(txt)
+            
+            # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+        except:
+            cria_audio("Verifique sua conexão ou api-key...")
+            time.sleep(3)
+            # ouvir_microfone()
     else:
 
-        txt = "Oi Gemini, hoje está um belo dia..."
+        cria_audio("Digite alguma coisa!")
 
-    try:
-        # Após alguns segundos, retorna a frase falada                   
-        gemini_ai(txt)
-        
-        # Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
-    except:
-        cria_audio("Verifique sua conexão ou api-key...")
-        time.sleep(3)
-        # ouvir_microfone()
+# Função para limpar a caixa de texto
+def limpar_caixa_de_texto():
+    ocultar_output()
+    ui.txt_resposta.setText("")
+    ui.txt_chat.setText("")
+
+# Copiar a caixa de texto
+def copiar_a_caixa_de_texto():
+    texto = ui.txt_chat.toPlainText()
+    texto += ui.txt_resposta.toPlainText()
+    
+    os.system(f"echo '{texto}' | xclip -select clipboard")
+    
+    limpar_caixa_de_texto()
 
 
 if __name__ == "__main__":
@@ -95,9 +128,13 @@ if __name__ == "__main__":
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
 
+    ocultar_output()
+
     # botões
     ui.bt_enviar.clicked.connect(enviar_chat)
     ui.bt_usar_mic.clicked.connect(ouvir_microfone)
+    ui.bt_erase.clicked.connect(limpar_caixa_de_texto)
+    ui.bt_copy.clicked.connect(copiar_a_caixa_de_texto)
 
     MainWindow.show()
     sys.exit(app.exec())
